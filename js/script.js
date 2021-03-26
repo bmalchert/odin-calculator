8244f52daf84b35b2d30a30b4d06977b8f757158
Benjamin Malchert <b.malchert0@gmail.com>
2021-03-26 11:35:20 -0700

    Add operate function and constants

--- js/script.js
+++ js/script.js
@@ -17,3 +17,43 @@ function multiply(x, y) {
 function divide(x, y) {
   return x / y;
 }
+
+// ******************************
+// Operation function
+//
+// Accepts two operands and an operator and calls the appropriate math function
+// ******************************
+
+const   ADD   =   'add';
+const   SUB   =   'subtract';
+const   MUL   =   'multiply';
+const   DIV   =   'divide';
+const   OPERR =   'invalid operator';
+
+function operate(x, y, op) {
+  let value;
+
+  switch (op) {
+    case 'add':
+      value = add(x, y);
+      break;
+
+    case 'subtract':
+      value = subtract(x, y);
+      break;
+
+    case 'multiply':
+      value = multiply(x, y);
+      break;
+
+    case 'divide':
+      value = divide(x, y);
+      break;
+
+    default:
+      value = OPERR;
+      break;
+  }
+
+  return value;
+}
