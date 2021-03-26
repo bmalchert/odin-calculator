706a99e2ac2cd5956eb8f9c066bbc37ee12ae87f
Benjamin Malchert <b.malchert0@gmail.com>
2021-03-26 11:09:31 -0700

    Add basic math functions

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// ******************************
+// Basic math functions
+// ******************************
+
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
